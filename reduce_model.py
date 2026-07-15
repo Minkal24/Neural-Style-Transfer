a4357893e238e2003f63f6a2abd105a17b081db1
Minkal Katariya <minkalkatariya@gmail.com>
2026-07-15 15:21:48 +0530

    Update model conversion script

--- reduce_model.py
+++ reduce_model.py
@@ -1,14 +1,14 @@
 import torch
 from utils.model import VGGEncoder
 
-print("Loading VGG...")
+print("Loading original VGG...")
 
 encoder = VGGEncoder("vgg_normalised.pth")
 
-print("Saving smaller VGG...")
+print("Saving only VGG layers...")
 
 torch.save(
-    encoder.state_dict(),
+    encoder.vgg.state_dict(),
     "vgg_encoder_small.pth"
 )
 
